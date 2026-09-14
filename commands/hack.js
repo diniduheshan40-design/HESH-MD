4a8f92528da221054fdd3c432d4da53ae809cce3
diniduheshan40-design <diniduheshan40@gmail.com>
2026-09-14 14:46:09 +0530

    Update hack.js

--- commands/hack.js
+++ commands/hack.js
@@ -1,63 +1,39 @@
-const {
-    smd,
-    prefix, 
-    Config ,
-    sleep
-     } = require('../lib')
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
+module.exports = {
+    name: 'hack',
+    async execute(sock, msg, args, sender) {
+        let sentMsg = await sock.sendMessage(sender, { 
+            text: "💻 *[ INITIATING CYBER ATTACK ]*\n\n💉 Injecting Malware..." 
+        }, { quoted: msg });
 
+        const steps = [
+            "💉 Injecting Malware...",
+            "█ 10%",
+            "█ █ 20%",
+            "█ █ █ 30%",
+            "█ █ █ █ 40%",
+            "█ █ █ █ █ 50%",
+            "█ █ █ █ █ █ 60%",
+            "█ █ █ █ █ █ █ 70%",
+            "█ █ █ █ █ █ █ █ 80%",
+            "█ █ █ █ █ █ █ █ █ 90%",
+            "█ █ █ █ █ █ █ █ █ █ 100%",
+            "⚙️ System hijacking in process...\n🌐 Connecting to remote server...",
+            "📡 Device successfully connected...\n📥 Receiving private data...",
+            "📂 Data extraction 100% completed!\n🧹 Killing evidence & removing malwares...",
+            "💀 *HACKING COMPLETED BY HESHAN-MD*",
+            "📤 Sending log documents to C2 server...",
+            "✅ Successfully sent data. Connection terminated!",
+            "🔒 *ALL BACKLOGS CLEARED & TRACES REMOVED!*"
+        ];
 
-
-
-
-
-smd({
-    cmdname: "hack",    
-    type: "fun",    
-    info: "hacking prank",    
-    filename: __filename,
-
-},
-
-async(citel) => {    
-await citel.send("Injecting Malware")   
-await sleep(2000)    
-await citel.send(" █ 10%")    
-await sleep(1000)    
-await citel.send(" █ █ 20%")    
-await sleep(1000)    
-await citel.send(" █ █ █ 30%")    
-await sleep(1000)    
-await citel.send(" █ █ █ █ 40%")    
-await sleep(1000)    
-await citel.send(" █ █ █ █ █ 50%")    
-await sleep(1000)    
-await citel.send(" █ █ █ █ █ █ 60%")    
-await sleep(1000)    
-await citel.send(" █ █ █ █ █ █ █ 70%")    
-await sleep(1000)    
-await citel.send(" █ █ █ █ █ █ █ █ 80%")    
-await sleep(1000)    
-await citel.send(" █ █ █ █ █ █ █ █ █ 90%")    
-await sleep(1000)    
-await citel.send(" █ █ █ █ █ █ █ █ █ █ 100%")    
-await sleep(1000)    
-await citel.send("System hyjacking on process.. \n Conecting to Server error to find 404 ")    
-await sleep(1000)    
-await citel.send("Device successfully connected... \n Riciving data...")    
-await sleep(1000)    
-await citel.send("Data hyjacked from divice 100% completed \n killing all evidence killing all malwares...")
-await sleep(1000)    
-await citel.send(" HACKING COMPLETED HESHAN MD ")    
-await sleep(2000)    
-await citel.send(" SENDING LOG DOCUMENTS...")    
-await sleep(1000)
-await citel.send(" SUCCESSFULLY SENT DATA AND Connection disconnected")    
-await sleep(1000)
-
-    return await citel.send('BACKLOGS CLEARED');
-
-}
-
-)
-
+        for (let step of steps) {
+            await delay(1000);
+            await sock.sendMessage(sender, {
+                text: `\`\`\`${step}\`\`\``,
+                edit: sentMsg.key
+            });
+        }
+    }
+};
