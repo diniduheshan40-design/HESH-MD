716362a82550438195664fea6d7044f5dced9528
diniduheshan40-design <diniduheshan40@gmail.com>
2026-09-14 15:55:29 +0530

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,20 +1,32 @@
 module.exports = {
     name: 'ping',
-    async execute(sock, msg, args, sender) {
+    async execute(sock, msg, args, targetChat) {
+        // පණිවිඩය ආපු නියම චැට් එකටම reply යැවීම තහවුරු කරයි
+        const chatJid = targetChat || msg.key.remoteJid;
         const start = Date.now();
 
-        // මුලින්ම යන කෙටි මැසේජ් එක
-        const sentMsg = await sock.sendMessage(sender, { 
-            text: "*Testing... ⚡*" 
-        }, { quoted: msg });
+        try {
+            // මුලින්ම යන කෙටි මැසේජ් එක
+            const sentMsg = await sock.sendMessage(chatJid, { 
+                text: "*Testing... ⚡*" 
+            }, { quoted: msg });
 
-        const latency = Date.now() - start;
+            const latency = Date.now() - start;
 
-        // Message එක edit කරලා speed එක පමණක් පෙන්වීම
-        await sock.sendMessage(sender, {
-            text: `*speed ${latency}ms 📍*`,
-            edit: sentMsg.key
-        });
+            // Message එක edit කරලා speed එක පෙන්වීම
+            try {
+                await sock.sendMessage(chatJid, {
+                    text: `*speed ${latency}ms 📍*`,
+                    edit: sentMsg.key
+                });
+            } catch (editErr) {
+                // Edit fail වුවහොත් අලුත් message එකක් ලෙස latency එක යවයි
+                await sock.sendMessage(chatJid, {
+                    text: `*speed ${latency}ms 📍*`
+                }, { quoted: msg });
+            }
+        } catch (err) {
+            console.error('Ping Command Error:', err);
+        }
     }
 };
-
