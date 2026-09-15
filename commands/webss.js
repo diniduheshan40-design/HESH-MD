8a6d9902466732e2ce01f8d4a74a459b18c70aa4
diniduheshan40-design <diniduheshan40@gmail.com>
2026-09-15 20:08:18 +0530

    Update webss.js

--- commands/webss.js
+++ commands/webss.js
@@ -1,27 +1,35 @@
 const axios = require('axios');
 
 module.exports = {
-    name: "ssweb",
-    category: "tools",
-    desc: "Take full webpage screenshot",
-    async run({ conn, m, args }) {
-        let url = args[0];
-        if (!url) return await m.reply("කරුණාකර Web Link එකක් ලබා දෙන්න!\nඋදා: `.ssweb https://google.com`");
-        if (!url.startsWith('http://') && !url.startsWith('https://')) url = 'https://' + url;
+  name: "ssweb",
+  category: "tools",
+  desc: "Take webpage screenshot",
 
-        try {
-            await m.reply("Capturing screenshot... 📸");
-            let imgUrl = `https://image.thum.io/get/width/1200/crop/800/fullpage/${url}`;
-            let res = await axios.get(imgUrl, { responseType: 'arraybuffer' });
+  // index.js එකේ format එකට 100% ගැලපෙන විදිහට arguments set කර ඇත
+  async run(sock, msg, args, chatJid, reply) {
+    try {
+      let url = args[0];
+      if (!url) {
+        return await reply("කරුණාකර Web Link එකක් ලබා දෙන්න!\nඋදා: `.ssweb https://google.com`");
+      }
 
-            await conn.sendMessage(m.chat, {
-                image: Buffer.from(res.data),
-                caption: `📸 *Screenshot Captured!*\n🌐 *URL:* ${url}`
-            }, { quoted: m });
-        } catch (err) {
-            console.error(err);
-            await m.reply("Screenshot ගැනීමට නොහැකි විය!");
-        }
+      if (!url.startsWith('http://') && !url.startsWith('https://')) {
+        url = 'https://' + url;
+      }
+
+      await reply("📸 Capturing screenshot, please wait...");
+
+      // Ultra-fast screenshot endpoint (Buffer එකක් නැතුව direct stream වේ)
+      const imgUrl = `https://image.thum.io/get/width/1280/crop/800/noanimate/${encodeURIComponent(url)}`;
+
+      await sock.sendMessage(chatJid, {
+        image: { url: imgUrl },
+        caption: `📸 *SCREENSHOT CAPTURED*\n\n🌐 *URL:* ${url}\n⚡ *Powered by HESHAN-MD*`
+      }, { quoted: msg });
+
+    } catch (err) {
+      console.error('ssweb error:', err.message);
+      await reply("❌ Screenshot ගැනීමට නොහැකි විය! Link එක නිවැරදිදැයි පරීක්ෂා කරන්න.");
     }
+  }
 };
-
