024fdf2da411d3c35d57e5051cf2525c78dc9c32
diniduheshan40-design <diniduheshan40@gmail.com>
2026-09-23 18:20:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ const { askAI } = require('./ai');
 
 const UPDATE_CHANNEL_JID = '120363421906774107@newsletter';
 const BOT_CHANNEL_NAME = '✗ ʜᴇꜱʜᴀɴ ᴏꜰᴄ ✨';
-const CHANNEL_REACTIONS = ['🩶', '💙', '❤️', '💛', '🧡', '💗', '🩵'];
+const CHANNEL_REACTIONS = ['😼', '🥰', '❤️', '😚', '👑', '💯', '👍'];
 const DEFAULT_BACKUP_LOGO = 'https://files.catbox.moe/a58add.jpeg';
 
 const channelContext = {
